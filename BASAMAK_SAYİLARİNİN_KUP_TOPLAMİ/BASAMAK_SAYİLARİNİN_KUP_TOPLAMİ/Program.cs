4abaaeba73b2ef6f4b28406cb03cc76a5c6bb731
Serkan <serkankabadayiogullari8233@gmail.com>
2023-09-17 01:04:21 +0300

    Basamak sayılarını kup toplami
    basamak sayılarının küpünü alıp toplayan kodu yazdım

---
+++ BASAMAK_SAYİLARİNİN_KUP_TOPLAMİ/BASAMAK_SAYİLARİNİN_KUP_TOPLAMİ/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace BASAMAK_SAYİLARİNİN_KUP_TOPLAMİ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("3 basamaklı bir sayı giriniz:");
+            int sayi=Convert.ToInt32(Console.ReadLine());
+           
+            if(Basamak(sayi))
+            {
+                int _sayi = sayi;
+                double kuptoplami = 0;
+                while(true)
+                {
+                    int basamak= _sayi % 10;
+                    _sayi /= 10;
+
+                    kuptoplami+=Math.Pow(basamak, 3);
+
+                    //371 sayısını deneyerek doğru sonucu bulabiliriz mesela
+                    if (_sayi < 10)
+                    {
+                        basamak = _sayi % 10;
+                        kuptoplami += Math.Pow(basamak, 3);
+                        break;
+                    }
+                }
+                if(kuptoplami==sayi)
+                {
+                    Console.WriteLine("eşittir");
+                }
+                else
+                    Console.WriteLine( "eşit değildir");
+            }
+            else
+                Console.WriteLine("lütfen sadece 3 basamaklı bir sayı giriniz:");
+
+        }
+        static bool Basamak(int sayi)
+        {
+            int basamaksayisi = 1;
+            while (true)
+            {
+                basamaksayisi++;
+                sayi /= 10;
+                if (sayi < 10)
+                    break;
+            }
+            if(basamaksayisi == 3)
+            {
+                return true;
+            }
+            else
+                
+            return false;
+        }
+    }
+}
